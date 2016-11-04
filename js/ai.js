a71d861b1ebb5fadf83f8c352a2175632bdbc04f
wanglangzhe <lu.shandi@163.com>
2016-11-04 17:36:15 +0900

    alpha-beta search
    注释了记忆化搜索部分、增加了ab剪枝的搜索

--- js/ai.js
+++ js/ai.js
@@ -3,54 +3,116 @@ var hash = new Array();
 var ai_level = 2;
 var player_turn = 1;
 var com_turn = 0;
-var ai_strategy
+var inf = 0x3f3f3f3f;
 // ai global var end
-/*
-$(document).ready(function () {
-	console.log('ai init');
-	
-	var fun = chess_box_click;
-	
-	console.log(fun);
-	
-	function for_switch_ai_control() {
-		console.log('swith success');
-		ai_control();
-		fun();
+
+//-1 not finish
+// 0 player1 win
+// 1 palyer2 win
+// 2 draw
+
+
+function check_state(side) {
+  for (var i = 1; i <= 2; ++ i) {
+	if (side[1] == i && side[2] == i && side[3] == i) {if (i-1 == com_turn) return 1; else return -1; } //电脑赢估价最大
+    if (side[4] == i && side[5] == i && side[6] == i) {if (i-1 == com_turn) return 1; else return -1; }
+    if (side[7] == i && side[8] == i && side[9] == i) {if (i-1 == com_turn) return 1; else return -1; }
+	                                                       
+    if (side[1] == i && side[4] == i && side[7] == i) {if (i-1 == com_turn) return 1; else return -1; }
+    if (side[2] == i && side[5] == i && side[8] == i) {if (i-1 == com_turn) return 1; else return -1; }
+    if (side[3] == i && side[6] == i && side[9] == i) {if (i-1 == com_turn) return 1; else return -1; }
+	                                                      
+    if (side[1] == i && side[5] == i && side[9] == i) {if (i-1 == com_turn) return 1; else return -1; }
+    if (side[3] == i && side[5] == i && side[7] == i) {if (i-1 == com_turn) return 1; else return -1; }
+  }  
+  for (var i = 1; i < 10; ++ i) if (side[i] == 0) return -inf; //不是最终态
+  return 0;	//平局
+}
+
+// 0 player1 win
+// 1 player2 win
+// 2 draw
+
+function min(a, b) { if (a<b) return a; else return b; }
+function max(a, b) { if (a>b) return a; else return b; }
+
+function dfs(state, player, alpha, beta) {
+	var res = check_state(state);
+	if (res != -inf) return res;
+	for (var i = 1; i < 10; ++i) if (state[i] == 0) {
+		state[i] = player + 1;
+		var v = dfs(state, 1 - player, alpha, beta);
+		state[i] = 0;
+		
+		if (player == com_turn) alpha = max(alpha, v);
+		else beta = min(beta, v);
+		
+		if (beta <= alpha) break;
+		
 	}
-	
-	chess_box_click = for_switch_ai_control;
-	
-	console.log(chess_box_click);
-	console.log(fun);
-	
-	ai_control();
-	
-	console.log('ai init end');
-	
-	var fun2 = restart;
-	
-  for (var i = 1; i <= 9; ++ i) { 
-    var box = $("<div>").addClass("chess-box");
-    box.attr("pos", i);
-    $('.chess').append(box);
-  }
+	if (player == com_turn) return alpha;
+	return beta;
+}
 
-  $('.chess-box').click(chess_box_click);
-  set_turn_info();
-  ai_control();
-  
-  
-   var f = .click
-   function l() {
-     f();
-	 awddfsaf
-   }	   
-   click = l;
-  
-});
-*/
+function ai_control() {
+	console.log(turn, com_turn, player_turn);
+	
+	if (turn != com_turn) return ;
+	
+	console.log('ai_control');
+	
+	if (ai_level == 1) { //级别1的AI，乱走一步
+		for (var i = 1; i < 10; ++i) {
+			if (typeof side[i] == 'undefined' || (typeof side[i] == 'number' && side[i] == 0)) {
+				$("div.chess-box:eq(" + (i-1) + ")").trigger("click");
+				break;
+			}
+		}	 
+	}
+	else {		//级别2AI，用alpha-beta剪枝进行搜索策略
+		var t = 0, tt = 0;
+		var state = new Array();
+		for (var i = 0; i < 10; ++i) {
+			state[i] = side[i];
+			if (typeof state[i] == 'undefined') state[i] = 0;
+		}
+		for (var i = 1; i < 10; ++i) {
+			var sel = -1;
+			if (typeof state[i] == 'undefined' || (typeof state[i] == 'number' && state[i] == 0)) {
+				tt = i;
+				
+				state[i] = com_turn + 1;
+				var sw = dfs(state, player_turn, -inf, inf);
+				state[i] = 0;
+				
+				if (sw == 1) { //能必胜直接退出
+					t = i;
+					break;
+				}
+				else if (sw == 0)	//不然保留一个平局态
+					t = i;
+			}
+		}
+		
+		if (t == 0) t = tt;	//若必败，选第一步可行走
+		$("div.chess-box:eq(" + (t-1) + ")").trigger("click");
+	}
+}
 
+
+
+
+
+/*  下面注释部分为原来旧版的记忆化搜索。上方是alpha—beta剪枝部分
+
+
+
+// ai global var
+var hash = new Array();
+var ai_level = 2;
+var player_turn = 1;
+var com_turn = 0;
+var ai_strategy
 //////////////////////////
 
 function get_key(state) {
@@ -113,7 +175,6 @@ function dfs(state, who, dep) {
 	var res = -1;
 	if (get_hash(state) != -1) return get_hash(state);
 	
-	//console.log(state, ' ', who, check(state));
 	var sw = check_state(state);
 	if (sw == 2) res = 2;
 	else if (sw == -1) {
@@ -144,26 +205,21 @@ function dfs(state, who, dep) {
 	
 	add_hash(state, res);
 	
-	//console.log(state, ' ', res, ' ', who, ' ', sel);
-	
 	return res;
 }
 
 function ai_control() {
 	if (turn != com_turn) return ;
-	//console.log('ai_control');	
 	if (ai_level == 1) {
 		for (var i = 1; i < 10; ++i) {
 			console.log(i, ' ', side[i], ' ', typeof side[i]);
 			if (typeof side[i] == 'undefined' || (typeof side[i] == 'number' && side[i] == 0)) {
-				console.log('ai_level 1  ', i, ' ', $(".chess-box").html());
 				$("div.chess-box:eq(" + (i-1) + ")").trigger("click");
 				break;
 			}
 		}	 
 	}
 	else {
-		//console.log('init');
 		var t = 0, tt = 0;
 		var state = new Array();
 		for (var i = 0; i < 10; ++i) state[i] = side[i];
@@ -175,7 +231,6 @@ function ai_control() {
 				var sw = dfs(state, player_turn, com_turn);
 				state[i] = 0;
 				
-				//console.log("sel: ", i, ' ', "sw: ", sw, ' ', com_turn);
 				
 				if (sw == com_turn) {
 					t = i;
@@ -187,11 +242,8 @@ function ai_control() {
 		}
 		
 		if (t == 0) t = tt;
-		//console.log(tt);
-		//console.log('final select  ', t);  ////fuck
-		//if (t > 0) 
 		$("div.chess-box:eq(" + (t-1) + ")").trigger("click");
-		//else 
-		//	$("div.chess-box:eq(" + (t-1) + ")").trigger("click");
 	}
 }
+
+*/
